97cad546af4d0c60ee63f7066d91d941571de120
Mahsan-code <mahsanpan@gmail.com>
2021-01-13 20:56:14 +0000

    call api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request');
-const nameOFBreed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${nameOFBreed}`, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode);
-   if(error){
-       console.log("PAGE NOT FOUND");
-   } 
- 
-  const data = JSON.parse(body);
-  if(data.length === 0){
-      console.log("BREED NOT FOUND");
-  }
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function (breedName, callback) {
+
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode);
+
+        if (error) {
+            callback("PAGE NOT FOUND", null);
+        }
+
+        const data = JSON.parse(body);
+        // console.log(data);
+        if (data.length === 0) {
+            callback("BREED NOT FOUND", null);
+        } else callback(null, data[0].description);
+    });
+};
+
+module.exports = { fetchBreedDescription }
